eaa559ff9c9ed2a01be4cbdf66247e6d7171fda1
Freja <143435200+yuki9k@users.noreply.github.com>
2025-09-11 19:45:38 +0200

    VBO and process input

--- src/main.c
+++ src/main.c
@@ -5,22 +5,27 @@
 // clang-format on
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void process_input(GLFWwindow *window);
 
 int main(void) {
-  const int winWidth = 800;
-  const int winHeight = 600;
+  // clang-format off
+  const float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f, 0.0f
+  };
+  // clang-format on
+
+  const int win_width = 800;
+  const int win_height = 600;
 
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  // #ifdef __APPLE__
-  //   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  // #endif
-
   GLFWwindow *window =
-      glfwCreateWindow(winWidth, winHeight, "LearnOpenGL", NULL, NULL);
+      glfwCreateWindow(win_width, win_height, "LearnOpenGL", NULL, NULL);
 
   if (window == NULL) {
     printf("Failed to create GLFW window");
@@ -36,7 +41,17 @@ int main(void) {
     return -1;
   }
 
+  // TODO:
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
   while (!glfwWindowShouldClose(window)) {
+    process_input(window);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
@@ -47,4 +62,10 @@ int main(void) {
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, 1);
+  }
 }
