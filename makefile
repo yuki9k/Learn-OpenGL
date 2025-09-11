4e6c843d862a6c309ac904e497df020c4cad8b5d
Freja <143435200+yuki9k@users.noreply.github.com>
2025-09-11 11:31:01 +0200

    makefile, included GLAD, modified gitignore and main.c

---
+++ makefile
@@ -0,0 +1,21 @@
+CC      = clang
+CFLAGS  = -Wall -Iinclude
+LDFLAGS =
+LDLIBS  = -lglfw
+SRCS    = src/main.c src/glad.c
+OBJS    = $(SRCS:.c=.o)
+TARGET  = bin/learnopengl
+
+all: $(TARGET)
+
+$(TARGET): $(OBJS) | bin
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $(OBJS) $(LDLIBS)
+
+src/%.o: src/%.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+bin:
+	mkdir -p bin
+
+clean:
+	rm -f $(OBJS) $(TARGET)
--- src/main.c
+++ src/main.c
@@ -1,1 +1,50 @@
+// clang-format off
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stdio.h>
+// clang-format on
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+int main(void) {
+  const int winWidth = 800;
+  const int winHeight = 600;
+
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // #ifdef __APPLE__
+  //   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  // #endif
+
+  GLFWwindow *window =
+      glfwCreateWindow(winWidth, winHeight, "LearnOpenGL", NULL, NULL);
+
+  if (window == NULL) {
+    printf("Failed to create GLFW window");
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    printf("Failed to initialize GLAD");
+    return -1;
+  }
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
